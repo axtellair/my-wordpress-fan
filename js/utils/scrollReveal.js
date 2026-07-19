5ab37d92f7521c07d9f6ca19f915a7e5766e5619
axtellair <axtellair@gmail.com>
2026-07-19 09:32:59 +0800

    Update scrollReveal.js

--- js/utils/scrollReveal.js
+++ js/utils/scrollReveal.js
@@ -1,1 +1,12 @@
-
+// 创建滚动入场观察器
+export function createRevealObserver() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(en => {
+      if (en.isIntersecting) {
+        en.target.classList.add('in');
+        observer.unobserve(en.target);
+      }
+    });
+  }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+  return observer;
+}
