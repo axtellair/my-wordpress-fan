8d93e752bcf5c08fe1aa426fdd47a91ee4bda348
axtellair <axtellair@gmail.com>
2026-07-19 09:32:35 +0800

    Update commpoentLoader.js

--- js/utils/commpoentLoader.js
+++ js/utils/commpoentLoader.js
@@ -1,1 +1,6 @@
-
+// 通用异步加载HTML组件工具
+export async function loadComponent(containerId, componentPath) {
+  const res = await fetch(componentPath);
+  const html = await res.text();
+  document.getElementById(containerId).innerHTML = html;
+}
